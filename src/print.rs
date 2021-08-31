46eb5bbf4eda83c120924203375eed16bb414553
samsif <samet.development@gmail.com>
2021-08-31 16:22:16 +0100

    print

---
+++ src/print.rs
@@ -0,0 +1,10 @@
+pub fn run() {
+    println!("hello from print rs");
+
+    println!("Number is: {}",5);
+    println!("binary is: {:b} Hex is: {:x} octal: {:o}",852, 852, 852);
+
+    println!("{:?}",(12, true, "test"));
+
+
+}
